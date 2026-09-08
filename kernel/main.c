0a15279b9e82862301b6e813c3e1665710f67e52
Dan3224 <Dan3224@users.noreply.github.com>
2026-09-08 17:33:41 +0300

    kernel: add first bootable MyOS prototype

---
+++ kernel/main.c
@@ -0,0 +1,31 @@
+static void outb(unsigned short port, unsigned char value) {
+    __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+static unsigned char inb(unsigned short port) {
+    unsigned char value;
+    __asm__ volatile ("inb %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
+static void serial_init(void) {
+    outb(0x3F8 + 1, 0x00);
+    outb(0x3F8 + 3, 0x80);
+    outb(0x3F8 + 0, 0x03);
+    outb(0x3F8 + 1, 0x00);
+    outb(0x3F8 + 3, 0x03);
+    outb(0x3F8 + 2, 0xC7);
+    outb(0x3F8 + 4, 0x0B);
+}
+
+static void serial_write(const char *text) {
+    while (*text) {
+        while ((inb(0x3F8 + 5) & 0x20) == 0) { }
+        outb(0x3F8, (unsigned char)*text++);
+    }
+}
+
+void kmain(void) {
+    serial_init();
+    serial_write("\nMyOS 0.1: booted successfully.\n");
+}
